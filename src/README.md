c4ac8f4ac939529d09cf36a74afe87da60293236
Anton Barinov <barinov@student.tugraz.at>
2018-09-08 18:31:09 +0200

    Added new filed String and Constructor to SomeClass.java

--- src/README.md
+++
@@ -1,3 +0,0 @@
-Readme file. 
-This is my learning project for version system GIT.
-Edit it if needed.
--- src/com/company/some_package/SomeClass.java
+++ src/com/company/some_package/SomeClass.java
@@ -1,4 +1,10 @@
 package com.company.some_package;
 
 public class SomeClass {
+
+    String string;
+
+    public SomeClass(String string) {
+        this.string = string;
+    }
 }
