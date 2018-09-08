b2e41c770618ce2656147f3c95c66cf7eda53aa2
Anton Barinov <barinov@student.tugraz.at>
2018-09-08 18:21:47 +0200

    Added new string to Main.java class. Printed new string.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello wonderful world");
-
+        String string = "Some string";
+        System.out.println(string);
     }
 }
