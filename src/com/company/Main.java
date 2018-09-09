9b487df70dd000d328a772702354aa3b08421692
Anton Barinov <barinov@student.tugraz.at>
2018-09-09 11:22:49 +0200

    Added '!' to world message.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
     private static final String constant = "MAIN HUMAN";
 
     public static void main(String[] args) {
-        String world = "Hello mad world";
+        String world = "Hello mad world!";
         System.out.println(world);
         System.out.println(new Human("Alex", "Armistrong", 23).toString());
         System.out.println(constant);
