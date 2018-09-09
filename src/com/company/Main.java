6550b0e261b121e7765d8c523f231166d8fbb1bc
Anton Barinov <barinov@student.tugraz.at>
2018-09-09 08:50:32 +0200

    Added constant string to Main.java class.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,9 +2,10 @@ package com.company;
 
 public class Main {
 
+    private static final String constant = "MAIN";
+
     public static void main(String[] args) {
         System.out.println("Hello wonderful world");
-        String string = "Some string";
-        System.out.println(string);
+        System.out.println(constant);
     }
 }
