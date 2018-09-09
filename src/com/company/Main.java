2d9e2ea3d007b6f15cf9dad1d5930641dd615f36
Anton Barinov <barinov@student.tugraz.at>
2018-09-09 09:40:49 +0200

    Changed surname and age of Alex.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         String world = "Hello mad world";
         System.out.println(world);
-        System.out.println(new Human("Alex", "Armstrong", 33).toString());
+        System.out.println(new Human("Alex", "Armistrong", 23).toString());
         System.out.println(constant);
     }
 }
