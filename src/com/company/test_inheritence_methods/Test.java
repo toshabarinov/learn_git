3796938e44d5af0a4b095ccf232416ff34fa3d3e
Anton Barinov <barinov@student.tugraz.at>
2018-09-09 08:30:31 +0200

    Added test file for test_inheritance_methods

---
+++ src/com/company/test_inheritence_methods/Test.java
@@ -0,0 +1,8 @@
+package com.company.test_inheritence_methods;
+
+public class Test {
+    public static void main(String[] args) {
+        Parent child = new Child();
+        child.method();
+    }
+}
