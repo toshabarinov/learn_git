d4c807075719f8a70bdeb9874244244e8a4b8452
Anton Barinov <barinov@student.tugraz.at>
2018-09-09 08:55:16 +0200

    Added new class Human.java to describe a human. New isntace of Human was printed in Main.java.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,14 @@
 package com.company;
 
+import com.company.instances.Human;
+
 public class Main {
 
     private static final String constant = "MAIN";
 
     public static void main(String[] args) {
         System.out.println("Hello wonderful world");
+        System.out.println(new Human("Alex", "Armstrong", 39).toString());
         System.out.println(constant);
     }
 }
